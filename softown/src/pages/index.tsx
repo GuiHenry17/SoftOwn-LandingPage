f9aff01e6f7019e57bc40ff53c9ca13ddb443d9d
GuiHenry17 <gui201629@gmail.com>
2024-10-16 23:18:49 -0300

    Dia 1

---
+++ softown/src/pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+    
+      <div className='defaultbg'>
+        <img src="/assets/logo-white.png"></img>
+        <div>
+          <button>Acessar o <strong>Sistema</strong></button>
+        </div>
+      </div>
+      <div style={{height: '299px'}}></div>
+    </div>
+  );
+};
